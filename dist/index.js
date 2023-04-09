1ec8c25c45c2aad55dd6f3a026042de64195e082
Russel Musasa <russelmusasa@gmail.com>
2023-04-09 12:24:22 +0100

    Typescript - Type Alias - Intersection

--- dist/index.js
+++ dist/index.js
@@ -6,29 +6,15 @@ console.log(point1.x);
 point1.x = 9;
 point1.y = 5;
 point1.draw();
-let employee = {
-    id: 2,
-    name: "Russel",
-    age: 20,
-    retire: (date) => {
-        console.log(date);
+let texBox = {
+    drag: () => {
+        console.log("Drag me");
+    },
+    resize: () => {
+        console.log("Resize me");
     }
 };
-console.log(employee);
-let mycolor = 'blue';
-console.log(mycolor);
-function kgToMiles(value) {
-    if (typeof value === 'number') {
-        console.log(typeof value);
-        return value * 2.2;
-    }
-    else {
-        console.log(typeof value);
-        return parseInt(value) * 2.2;
-    }
-}
-let responseConverted1 = kgToMiles(26);
-console.log(responseConverted1);
-let responseConverted2 = kgToMiles('26');
-console.log(responseConverted2);
+console.log(texBox);
+let quantity = 50;
+console.log("Quantity" + quantity);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,41 +8,25 @@ point1.y = 5;
 point1.draw();
 
 // Type Alias
-type Employee = {
-    readonly id: number, 
-    name: string, 
-    age: number,
-    retire: (date: Date) => void 
-}
+// Intersection
+type Draggable = {
+    drag: () => void
+};
 
-type Color = 'red' | 'white' | 'blue';
+type Resizable = {
+    resize: () => void
+};
 
-// Objects
-let employee: Employee = { 
-    id: 2, 
-    name: "Russel", 
-    age: 20,
-    retire: (date: Date) => {
-        console.log(date);
-    }
- };
+type UiWidget = Draggable & Resizable;
 
-console.log(employee);
-
-// using color type
-let mycolor: Color = 'blue';
-console.log(mycolor);
-
-function kgToMiles(value: number | string) {
-    if(typeof value === 'number') {
-        console.log(typeof value);
-        return value * 2.2;
-    } else {
-        console.log(typeof value);
-        return parseInt(value) * 2.2
+let texBox: UiWidget = {
+    drag: () => {
+        console.log("Drag me");
+    },
+    resize: () => {
+        console.log("Resize me");
     }
 }
-let responseConverted1 = kgToMiles(26);
-console.log(responseConverted1);
-let responseConverted2 = kgToMiles('26');
-console.log(responseConverted2);
+console.log(texBox);
+
+
