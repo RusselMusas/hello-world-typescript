9e147b09443b5f24b5fe7eb88533277402ae14ef
Russel Musasa <russelmusasa@gmail.com>
2023-04-10 16:28:32 +0100

    Typescript - Class

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const point_1 = require("./point");
-let point1 = new point_1.Point(7, 3);
-console.log(point1.x);
-point1.x = 9;
-point1.y = 5;
-point1.draw();
-let speed = null;
-let ride = {
-    speed: speed !== null && speed !== void 0 ? speed : 30
-};
-console.log(ride);
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this.balance = amount;
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { Point } from './point';
+class Account {
+    id: number;
+    owner: string;
+    balance: number;
 
-// Invocation
-let point1 = new Point(7, 3);
-console.log(point1.x);
-point1.x = 9;
-point1.y = 5;
-point1.draw();
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
 
-// Nullish coalescing operator
-let speed: number | null = null;
-
-let ride = {
-    // Falsy values in js (undefined, false, '', 0, null)
-    // Nullish coalescing operator: ?? (only takes null or undefined)
-    speed: speed ?? 30
+    deposit(amount: number): void {
+        if(amount <= 0)
+            throw new Error('Invalid amount');
+        this.balance = amount;
+    }
+    
 }
-console.log(ride);
